a1366e9499a69045328be45586b30e70ab0f3c83
Mark Briscoe <mark@mcmw.co.uk>
2024-11-04 16:07:36 +0000

    add: line-through class on delete

---
+++ app.js
@@ -0,0 +1,7 @@
+let a = 8;
+let b = 6;
+
+let x = a > 6 ? a % 2 === 0 ? "even" : "odd" : false;
+
+console.log(x);
+
--- script.js
+++ script.js
@@ -1,41 +1,39 @@
-//create references to html elements
-const addToListBtn = document.querySelector('#addToListButton');
-const listTextInput = document.querySelector('#listTextInput');
-const myList = document.querySelector('#myList');
-const title = document.querySelector('h1');
+// SET UP VARIABLES
+const addToListBtn = document.querySelector("#addToListButton");
+const listTextInput = document.querySelector("#listTextInput");
+const myList = document.querySelector("#myList");
+const title = document.querySelector("h1");
 
-title.style.backgroundColor = 'blue';
-title.style.color = 'white';
+title.style.backgroundColor = "blue";
+title.style.color = "white";
 
-addToListBtn.addEventListener('click', addToList);
+// ADD EVENT LISTENER
+addToListBtn.addEventListener("click", addToList);
 
-function addToList() {
-    // check if there's text in the box
-    if (listTextInput.value) {
-        // if there is add it to the list
-        const newListElement = document.createElement('li');
-        newListElement.textContent = listTextInput.value;
-
-        //add an event listener to remove the item when clicked
-        newListElement.addEventListener('click', removeFromList);
-
-        // clear the box
-        listTextInput.value = '';
+function addToList(){
+    // check if there's text in the text box
+    if(listTextInput.value){
 
         // update the list
+        const newListElement = document.createElement("li");
+        newListElement.textContent = listTextInput.value;
+        newListElement.addEventListener("click", removeFromList) 
         myList.appendChild(newListElement);
 
-        // focus back on text box
+        // clear the text from the text box
+        listTextInput.value = '';
+
+        // set focus to text box
         listTextInput.focus();
-    }
+    }    
 }
 
-// remove an item from the list
-function removeFromList(e) {
-    if (confirm('Are you sure you want to remove this item?')) {
-        const listElement = e.target;
-        listElement.parentNode.removeChild(listElement);
-        // focus back on text box
-        listTextInput.focus();
+function removeFromList(e){
+    if(confirm("Are you sure?")){
+    const listElement = e.target;
+    listElement.parentNode.removeChild(listElement);
+    // listElement.style.textDecoration = "line-through";
+    listElement.style.color = "red";
+    listTextInput.focus()
     }
 }
